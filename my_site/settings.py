a602f8dcc05698792f3b55764c4f5b3fc7075e13
jhaharsh03 <harshj92002@gmail.com>
2023-07-01 12:41:21 +0530

    db is added to fetch the post

--- my_site/settings.py
+++ my_site/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_max',
+        'USER': 'glam1',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
